33f77760e3a6cc079d98023b177c195f2839445d
tsheez <t2shaw@ucsd.edu>
2016-02-03 17:10:51 -0800

    Added sexual recombination to evolution python script

---
+++ weeks/week05/sex.py
@@ -0,0 +1,63 @@
+from random import choice, random
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz '
+
+def initialize(parent, num):
+    """Returns num random sequences the length of parent."""
+    return [[choice(ALPHABET) for i in range(len(parent))] for seq in range(num)]
+
+def score(seq, target):
+    """Returns number of differences between seq and target."""
+    return sum(map(int, [a != b for a, b in zip(seq, target)]))
+
+def select(population, scores):
+    """Returns best sequence and score from population."""
+    scored = zip(scores, population)
+
+    #Changed from scored.sort()
+    scored = sorted(scored)
+    return scored[0]
+
+def breed(parent, num, mutation_rate):
+    """Returns num copies of parent with mutation_rate changes per letter."""
+    result = []
+    length = len(parent)
+    for seq in range(num):
+        curr = parent[:]
+        for pos in range(length):
+            if random() <= mutation_rate: curr[pos] = choice(ALPHABET)
+        result.append(curr)
+    return result
+
+def sex(population, scores):
+    """Takes top two population members, makes two children, and picks the fittest between children and parents"""
+    result = []
+    scored = zip(scores, population)
+    scored = sorted(scored)
+    mama = scored[0][1]
+    papa = scored[1][1]
+    result.append(mama); result.append(papa)
+    length = len(mama)
+    index = int(length/2)
+    baby=mama[:index] + papa[index:]
+    result.append(baby)
+    baby2 = papa[:index] + mama[index:]
+    result.append(baby2)
+    return result
+
+def evolve(target, num=1000,  mutation_rate=0.01, generation=0):
+    """Evolves random sequences towards seed, using ALPHABET."""
+    population = initialize(target, num)
+    while 1:
+        scores = [score(seq, target) for seq in population]
+        sexedPop = sex(population, scores)
+        scores = [score(seq, target) for seq in sexedPop]
+        best_score, best_seq = select(sexedPop, scores)
+
+        #Changed to py3 print function
+        print (generation, '\t', best_score, '\t', ''.join(best_seq))
+        if best_score == 0: break
+        population = breed(best_seq, num, mutation_rate)
+        generation += 1
+if __name__=='__main__':
+    evolve('its the remix to ignition hot and fresh out the kitchen')
